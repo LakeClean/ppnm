f588a97cae64517b81ff9c07636d1893fe657f97
LakeClean <soeren2609@icloud.com>
2024-02-14 15:25:03 +0100

    finished vec

--- exercises/vec/main.cs
+++ exercises/vec/main.cs
@@ -3,7 +3,36 @@ using static System.Console;
 public static class main{
     static void Main(){
         // Lets first test if we can make a vector:
-        var v_1 = new vec(1,2,3);
-        WriteLine($"{v_1}");
+        var v = new vec(1,2,3);
+	WriteLine($"Vector v=(1,2,3) is constructed: {v}");
+	
+	// Checking operators:
+	WriteLine($"10*v: {10*v}, v*10: {v*10}");
+	var u = new vec(4,5,6);
+	WriteLine($"We define additional vector u: {u}");
+	WriteLine($"v+u: {v+u}");
+	WriteLine($"-v: {-v}");
+	WriteLine($"v-u: {v-u}");
+
+	//Checking print method
+	v.print("v is equal to: ");
+	Write("We test the v.print() with no argument: ");
+	v.print();
+	
+	//Checking dot-product, vector-product, and norm methods:
+	WriteLine($"v.dot(u): {v.dot(u)}, should be equal to: 32");
+	WriteLine($"vec.dot(u,v): {vec.dot(u,v)}, should be equal to: 32");
+	vec w = vec.cross(v,u);
+	w.print("w is the cross product of u and v and is equal to: ");
+	WriteLine($"vec.norm(v): {vec.norm(v)}");
+	
+	//Checking approx method:
+	WriteLine($"vec.approx(u,v) is: {vec.approx(u,v)} and should be false");
+	vec d1 = new vec(0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1,0,0);
+	vec d2 = new vec(8*0.1,0,0);
+	WriteLine($"vec.approx(d1,d2) is: {vec.approx(d1,d2)} and should be true");
+	
+	//Checking ToString Method
+	WriteLine($"ToString of v returns: {v.ToString()}");
     }//Main
 }//main
--- exercises/vec/vec.cs
+++ exercises/vec/vec.cs
@@ -6,12 +6,14 @@ public class vec{
 	//Constructors
 	public vec(){x=y=z=0;}//vec constructor
 	public vec(double x,double y,double z){ this.x=x; this.y=y; this.z=z; }
-	//Overloading p
+	//Overloading operators
 	public static vec operator*(vec v, double c){return new vec(c*v.x,c*v.y,c*v.z);}
 	public static vec operator*(double c, vec v){return v*c;}
-	public static vec operator+(vec u, vec v){return new vec(u.x+v.x,u.y+v.y,u.z+v.z);}
+	public static vec operator + (vec u, vec v){return new vec(u.x+v.x,u.y+v.y,u.z+v.z);}
 	public static vec operator-(vec u){return new vec(-u.x,-u.y,-u.z);}
 	public static vec operator-(vec u, vec v){return new vec(u.x-v.x,u.y-v.y,u.z-v.z);}
+
+	
 	//methods:
 	public void print(string s){Write(s);WriteLine($"{x} {y} {z}");}
 	public void print(){this.print("");}
@@ -20,6 +22,19 @@ public class vec{
 		{return this.x*other.x+this.y*other.y+this.z*other.z;}
 	public static double dot(vec v,vec w) /* to be called as vec.dot(u,v) */
 		{return v.x*w.x+v.y*w.y+v.z*w.z;}
+	
+	public static vec cross(vec v, vec u){
+				double a = v.y*u.z - v.z*u.y;
+				double b = -(v.x*u.z - v.z*u.x);
+				double c = v.x*u.y - v.y*u.x;
+				return new vec(a,b,c);
+			}//cross
+	
+	public static double norm(vec v){
+				double v_dot_v = vec.dot(v,v);
+				return Sqrt(v_dot_v);
+			}// norm
+	
 
 	static bool approx(double a,double b,double acc=1e-9,double eps=1e-9){
 		if(Abs(a-b)<acc)return true;
